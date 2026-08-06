453b5298834565557120f049474e5d0b310f8a52
Fares Sabbah <f.sabbah@xocialive.com>
2026-08-06 14:44:44 +0300

    add theme toggle functionality and integrate next-themes for theme management

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,17 @@ import App from './App';
 
 import './i18n';
 import './index.css';
+import { ThemeProvider } from './components/theme/ThemeProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider
+      attribute='class'
+      defaultTheme='system'
+      enableSystem
+      disableTransitionOnChange
+    >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
